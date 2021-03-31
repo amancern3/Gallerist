249ea4912e44daf01203406dbbdca4e350525841
Amandeep Singh <amandeep842.as@gmail.com>
2021-03-31 02:48:28 -0700

    added some comments for image download phase

--- src/webscrapper.rs
+++ src/webscrapper.rs
@@ -17,7 +17,15 @@ pub async fn object_of_the_day(url: &str) {
     // impo ref: https://docs.rs/scraper/0.12.0/scraper/
 
     // Object of the day = oftd
-    let oftd = Selector::parse(".h2").unwrap();
+
+    // tutorial to download images:
+    // get img tag by parsing and then the img src by parsing
+    // use the collected h2 tag as filename
+    // write the link to the file ? 
+    // ref: https://www.youtube.com/watch?v=m_agcM_ds1c
+
+    // alt use crate image to write image buff 
+    let oftd = Selector::parse("h2").unwrap();
     println!("made it !");
 
     for oftd in body.select(&oftd){
