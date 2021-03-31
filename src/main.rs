6b79f35aeda5e4b0b07821c1dee63a0a50817609
amancern3 <amandeep842.as@gmail.com>
2021-03-31 00:29:38 -0700

    async behavior implemented to crawler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate serenity;
+extern crate scraper;
+extern crate tokio;
+
+use tokio::task; 
 
 use serenity::prelude::*;
 use serenity::model::gateway::Ready; 
 use serenity::model::channel::Message; 
 use serenity::async_trait; 
 
-use std::env; 
+mod webscrapper; 
 
 struct Handler;
 
@@ -17,6 +21,8 @@ impl EventHandler for Handler {
 
     async fn message(&self,_ctx: Context, _new_message: Message) {
         if _new_message.content == "!gallerist" {
+            
+            let _task = task::spawn(webscrapper::object_of_the_day("https://www.slam.org/explore-the-collection/object-of-the-day/")).await;
             if let Err(why) = _new_message.channel_id.say(&_ctx.http, "test success!").await {
                 println!("Error giving message: {:?}", why);
             }
